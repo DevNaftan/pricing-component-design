4a71408255069c3deb8781f20f7d1bcd077483d9
DevNaftan <x.X.Naftan.X.x@gmail.com>
2020-08-14 11:51:48 -0500

    Finish

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+const toogle = document.getElementById('toogle')
+const plan1 = document.getElementById('plan-1')
+const plan2 = document.getElementById('plan-2')
+const plan3 = document.getElementById('plan-3')
+
+
+function mode(event) {
+  if (event.target.checked) {
+    plan1.textContent = '199.99'
+    plan2.textContent = '249.99'
+    plan3.textContent = '399.99'
+  } else {
+    plan1.textContent = '19.99'
+    plan2.textContent = '24.99'
+    plan3.textContent = '39.99'
+  }
+}
+
+toogle.addEventListener('change', mode)
