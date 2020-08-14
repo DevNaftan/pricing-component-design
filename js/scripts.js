0d4680ed30dca1cc79e99440d91ce5ec7e66a57f
DevNaftan <x.X.Naftan.X.x@gmail.com>
2020-08-14 12:24:29 -0500

    Change mode with Spacebar

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body')
 const toogle = document.getElementById('toogle')
 const plan1 = document.getElementById('plan-1')
 const plan2 = document.getElementById('plan-2')
@@ -15,5 +16,21 @@ function mode(event) {
     plan3.textContent = '39.99'
   }
 }
+
+body.addEventListener('keydown', event => {
+  if (event.code === 'Space') {
+    if (toogle.checked) {
+      toogle.checked = false
+      plan1.textContent = '19.99'
+      plan2.textContent = '24.99'
+      plan3.textContent = '39.99'
+    } else {
+      toogle.checked = true
+      plan1.textContent = '199.99'
+      plan2.textContent = '249.99'
+      plan3.textContent = '399.99'
+    }
+  }
+})
 
 toogle.addEventListener('change', mode)
